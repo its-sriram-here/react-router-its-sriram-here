c767d750536a6bf3686de7cfeddf30c9ca9fa3df
SRIRAM <srirammurugesan1807@gmail.com>
2025-09-04 09:50:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
-
 import React from "react";
 import './../styles/App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
 
-const App = () => {
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
