58de80e0bfc5f0253e64a66df456ec2a6ddd4059
SRIRAM <srirammurugesan1807@gmail.com>
2025-09-04 09:47:30 +0530

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>This is a sample React Router program.</p>
+    </div>
+  );
+}
+
+export default About;
